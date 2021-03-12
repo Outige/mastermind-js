86c45fd71a137c3b0f884683ef834ae00adee228
Tieg <tiegosullivanpsnl@gmail.com>
2021-03-12 21:35:14 +0200

    :sparkles: list items generated through javascript

--- Game.js
+++ Game.js
@@ -41,4 +41,85 @@ export default class Game {
         // TODO: also check if code is guessed correct. I think i've done so
         return  this.board.length >= this.height || this.calculateNumberOfCows(this.board[this.board.length-1]) === this.width;
     }
+
+    // DOM generation
+    generateCodeListFromBoard() {
+        let codeList = document.createElement('ul');
+        codeList.classList.add('code-list');
+
+        // Add past codes
+        for (let x of this.board) {
+            let codeListItem = document.createElement('li');
+            codeListItem.classList.add('code-list-item');
+            codeListItem.classList.add('past-code');
+
+            let codeInput = document.createElement('div');
+            codeInput.classList.add('code-input');
+            codeInput.innerText = `${x}`;
+
+            let confirmCcodeBtn = document.createElement('button');
+            confirmCcodeBtn.classList.add('confirm-code-btn');
+            confirmCcodeBtn.innerHTML = '<i class="fas fa-check-square"></i>';
+            confirmCcodeBtn.disabled = true;
+
+            codeListItem.appendChild(codeInput);
+            codeListItem.appendChild(confirmCcodeBtn);
+            codeList.appendChild(codeListItem);
+        }
+
+        // Add current codes
+        if (this.board.length < this.height -1) {
+            let codeListItem = document.createElement('li');
+            codeListItem.classList.add('code-list-item');
+
+            let codeInput = document.createElement('div');
+            codeInput.classList.add('code-input');
+            codeInput.innerText = `${'0000'}`;
+            codeInput.contentEditable = true;
+
+            let confirmCcodeBtn = document.createElement('button');
+            confirmCcodeBtn.classList.add('confirm-code-btn');
+            confirmCcodeBtn.innerHTML = '<i class="fas fa-check-square"></i>';
+
+            codeListItem.appendChild(codeInput);
+            codeListItem.appendChild(confirmCcodeBtn);
+            codeList.appendChild(codeListItem);
+        }
+
+        // Add future codes
+        for (let i = this.board.length+1; i < this.height; i++) {
+            let codeListItem = document.createElement('li');
+            codeListItem.classList.add('code-list-item');
+            codeListItem.classList.add('future-code');
+
+            let codeInput = document.createElement('div');
+            codeInput.classList.add('code-input');
+            codeInput.innerText = `${'0000'}`;
+
+            let confirmCcodeBtn = document.createElement('button');
+            confirmCcodeBtn.classList.add('confirm-code-btn');
+            confirmCcodeBtn.innerHTML = '<i class="fas fa-check-square"></i>';
+            confirmCcodeBtn.disabled = true;
+
+            codeListItem.appendChild(codeInput);
+            codeListItem.appendChild(confirmCcodeBtn);
+            codeList.appendChild(codeListItem);
+        }
+
+        // TODO: add the current and future codes
+        return codeList;
+    }
+
+    // DOM manipulation
+    refreshCodeListFromBoard(document) {
+        let codeList = this.generateCodeListFromBoard();
+
+        let gameContainer = document.getElementsByClassName('game-container')[0];
+        let oldCodeList = document.getElementsByClassName('code-list')[0];
+        console.log(oldCodeList);
+        oldCodeList.remove();
+        gameContainer.appendChild(codeList);
+        console.log(gameContainer);
+    }
+
 }
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import Game from './Game.js';
 
 let code = '2368'
-let game = new Game(4, 20, code);
+let game = new Game(4, 7, code);
 console.log(`code: ${game.code}`);
 
 console.log('\nmove 1')
@@ -10,3 +10,13 @@ game.addGuessToBoard(guess);
 console.log(`board ${game.board}`);
 console.log(`pigs: ${game.calculateNumberOfPigs(guess)}`);
 console.log(`cows: ${game.calculateNumberOfCows(guess)}`);
+
+console.log('\nmove 1')
+guess = '4568'
+game.addGuessToBoard(guess);
+console.log(`board ${game.board}`);
+console.log(`pigs: ${game.calculateNumberOfPigs(guess)}`);
+console.log(`cows: ${game.calculateNumberOfCows(guess)}`);
+
+
+game.refreshCodeListFromBoard(document);
